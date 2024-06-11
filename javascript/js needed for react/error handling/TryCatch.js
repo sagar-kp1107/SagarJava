9e252fd96e2b537a301868de2b80a8e3efd4b19f
sagar-kp1107 <135790952+sagar-kp1107@users.noreply.github.com>
2024-06-11 20:56:53 +0530

    Add files via upload

---
+++ javascript/js needed for react/error handling/TryCatch.js
@@ -0,0 +1,55 @@
+setTimeout(()=>{
+    console.log("hacking wifi .....plz wait....")
+},1000)
+
+
+
+
+try{
+console.log(rahul)
+}
+catch(err){
+    console.log("balla balla")//matlab error aya to isa print kardo
+}
+
+
+// try {
+//     try the code
+// }catch(error) {                    //this error var contains an error object (matlab yaha err ka jagha pa tum
+//     error handling                 ////kuch bhi likh sakta ho)
+// }
+
+
+//this will give an error which will the entire furthur code from running to avoid this we use try and catch (matlab ya try karo iss code ko ya to catch matlab agar na ho to wo kar do jo catch ma likha hai)
+
+
+setTimeout(()=>{
+    console.log("fetching the user name and passawrd .....plz wait....")
+},2000)
+
+setTimeout(()=>{
+    console.log("hacking rahuls facebook id .....plz wait....")
+},3000)
+
+setTimeout(()=>{
+    console.log("hacking rahuls instagram id .....plz wait....")
+},4000)
+
+
+
+
+
+
+
+
+
+
+
+
+//code jo ki run ya phir handle nahi honga try aur catch sa jaisa ki setTimeout  example
+setTimeout(()=>{
+    console.log(rahul)
+},10000)//matlab jab bhi future ma future matlab 10sec bad jab bhi ya code chalna ki kosis karaga tab isma aya hua error ki jimadari try aur catch ki nahi hai
+
+//setTimeout ka andar hum use kar sakta ha try aur  catch ko
+//ex : ajj humna kisiko klaha dudh ubal raha ha dakh lana kahi bahar gir na jaya aur humna usa 10sal bad call kiya aur kaha mana dudh ubala tha ajj gir gaya to phir wo uski galta thodi naa hai
